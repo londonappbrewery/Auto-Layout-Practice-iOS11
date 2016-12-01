38aa90d2ef873c4cb164baf24de5ae7a93a89e35
angelabauer <angela_yu@hotmail.co.uk>
2016-12-01 13:41:47 +0000

    first commit

---
+++ Auto Layout Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Angela Yu on 01/12/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
